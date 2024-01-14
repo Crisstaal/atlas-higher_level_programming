171b52530cdab3a8d16266ba9777807f2524085b
Crisstaal <144152402+Crisstaal@users.noreply.github.com>
2024-01-14 16:37:54 -0600

    data structures

---
+++ python-data_structures/8-multiple_returns.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def multiple_returns(sentence):
+    len = len(sentence)
+    if (len == 0):
+        new_tuple = (len None)
+    else:
+        new_tuple = (len, sentence[0])
+        return (new_tuple)
