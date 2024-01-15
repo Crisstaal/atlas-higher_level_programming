6e156e8157dc66d6f1dd9a7ad35c8d2f49d0b19d
“github <you@example.com>
2024-01-14 19:22:03 -0800

    returns

--- python-data_structures/8-multiple_returns.py
+++ python-data_structures/8-multiple_returns.py
@@ -2,7 +2,7 @@
 def multiple_returns(sentence):
     len = len(sentence)
     if (len == 0):
-        new_tuple = (len None)
+        new_tuple = (len(str(None)), )
     else:
         new_tuple = (len, sentence[0])
         return (new_tuple)
