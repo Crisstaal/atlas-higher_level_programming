3b0a2775f3818cc09ddfc576b0ce19a931d7ff0a
“github <you@example.com>
2024-01-14 20:12:22 -0800

    roman

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -2,7 +2,6 @@
 def roman_to_int(roman_string):
 if romans = {'I': 1, 'V': 5, 'X': 10, 'L':50, 'C': 100, 'D': 500, 'M': 1000,\
 	    'IV': 4, 'IX': 9, 'XL': 40, 'XC': 90, 'CD': 400, 'CM': 900}:
-
     if roman_string and type(roman_string) == str:
         number = 0
         for i in range(len(roman_string)):
@@ -15,4 +14,4 @@ if romans = {'I': 1, 'V': 5, 'X': 10, 'L':50, 'C': 100, 'D': 500, 'M': 1000,\
                     break
         return number
     else:
-        return 0
+    return 0
