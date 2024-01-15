426a1c28229723cf83d32db49c312c8065771afc
“github <you@example.com>
2024-01-14 19:12:51 -0800

    tuple

--- python-data_structures/7-add_tuple.py
+++ python-data_structures/7-add_tuple.py
@@ -2,6 +2,7 @@
 def add_tuple(tuple_a=(), tuple_b=()):
     len_a = len(tuple_a)
     len_b= len(tuple_b)
+    
     if len_a < 1:
         tuple_a = (0, 0)
     elif len_a < 2:
@@ -12,5 +13,5 @@ def add_tuple(tuple_a=(), tuple_b=()):
     elif len_b < 2:
         tuple_b = (tuple_b[0], 0)
 
-        new_tuple = (tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])
-        return new_tuple
+    new_tuple = (tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])
+    return new_tuple
