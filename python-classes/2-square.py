aa3dde168f241a64365389000e9a6e8f5b44307b
Crisstaal <144152402+Crisstaal@users.noreply.github.com>
2024-01-18 23:31:08 -0600

    test driven and more classes

---
+++ python-classes/2-square.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""defines a class Square"""
+
+
+class Square:
+    """defines a square
+    Attributes:
+        __size (int): size of side
+    """
+    def __init__(self, size=0):
+        """initialize square
+        Args:
+            size (int): size of a side
+        Returns:
+            None
+        """
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        else:
+            if size < 0:
+                raise ValueError("size must be >= 0")
+            else:
+                self.__size = size
