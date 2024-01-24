e0e49e3840f39cdc1c0139853c468fe4a93342d2
Crisstaal <144152402+Crisstaal@users.noreply.github.com>
2024-01-23 23:44:57 -0600

    fix errors

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -3,12 +3,6 @@
 
 
 def read_file(filename=""):
-<<<<<<< HEAD
-
-    with open(filename, 'r', encoding="utf-8") as f:
-        read_data = f.read()
-        print(read_data, end='')
-=======
     """Print the contents of a UTF8 text file to stdout."""
     with open(filename, encoding="utf-8") as f:
         print(f.read(), end="")
