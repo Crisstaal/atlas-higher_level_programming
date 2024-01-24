5db7b7545885eda6d050ab0bd8352e65ffae4456
Your Name <you@example.com>
2024-01-23 21:43:30 -0800

    python

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -12,4 +12,3 @@ def read_file(filename=""):
     """Print the contents of a UTF8 text file to stdout."""
     with open(filename, encoding="utf-8") as f:
         print(f.read(), end="")
->>>>>>> 9d5fdab78d1974c26e5cd9f4e39750ddcf9bf25a
