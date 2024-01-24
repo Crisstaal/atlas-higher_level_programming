9d5fdab78d1974c26e5cd9f4e39750ddcf9bf25a
Crisstaal <144152402+Crisstaal@users.noreply.github.com>
2024-01-23 23:34:53 -0600

    fix errors

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
-""" Module defines a text file-reading function """
+"""module defines a text file-reading function."""
+
 
 def read_file(filename=""):
-    """ Function that reads from a file """
-      with open(filename, 'r', encoding="utf-8") as f:
-        print(read_data, end='')
+    """Print the contents of a UTF8 text file to stdout."""
+    with open(filename, encoding="utf-8") as f:
+        print(f.read(), end="")
