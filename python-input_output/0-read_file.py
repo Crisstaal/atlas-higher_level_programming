c2bcdfefdb6c3d1d202c7abcd2fb8082f2326fe3
Crisstaal <144152402+Crisstaal@users.noreply.github.com>
2024-01-23 23:31:14 -0600

    commit

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -4,6 +4,5 @@
 
 def read_file(filename=""):
       """ Function that reads from a file """
-
-    with open(filename, 'r', encoding="utf-8") as f:
+      with open(filename, 'r', encoding="utf-8") as f:
         print(read_data, end='')
