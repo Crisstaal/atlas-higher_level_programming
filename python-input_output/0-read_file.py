923b05ac578c4eaff640fa0b7b846633b75f65b6
Crisstaal <144152402+Crisstaal@users.noreply.github.com>
2024-01-23 23:32:19 -0600

    fix error

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 """ Module defines a text file-reading function """
-
-
 def read_file(filename=""):
-      """ Function that reads from a file """
+    """ Function that reads from a file """
       with open(filename, 'r', encoding="utf-8") as f:
         print(read_data, end='')
