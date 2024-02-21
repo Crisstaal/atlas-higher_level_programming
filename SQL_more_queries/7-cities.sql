19acc31f496098d8c452beb485ea043a08c86fe2
Crisstaal <144152402+Crisstaal@users.noreply.github.com>
2024-02-21 00:21:46 -0600

    fix

--- SQL_more_queries/7-cities.sql
+++ SQL_more_queries/7-cities.sql
@@ -1,6 +1,11 @@
 -- creates the database hbtn_0d_usa and the table cities
+-- Create database if not exists
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 
+-- Switch to the database
+USE hbtn_0d_usa;
+
+-- Create the cities table if not exists
 CREATE TABLE IF NOT EXISTS cities (
     id INT AUTO_INCREMENT PRIMARY KEY,
     state_id INT NOT NULL,
