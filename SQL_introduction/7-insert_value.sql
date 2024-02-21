042d161962b0b81a78b413e69736d972dc02bc0a
Crisstaal <144152402+Crisstaal@users.noreply.github.com>
2024-02-20 22:59:33 -0600

    fix error

--- SQL_introduction/7-insert_value.sql
+++ SQL_introduction/7-insert_value.sql
@@ -1,12 +1,2 @@
 -- inserts a new row in the table first_table (database hbtn_0c_0) in your MySQL server
-
-USE hbtn_0c_0;
-
--- Check if the row with id 89 already exists
-IF NOT EXISTS (SELECT * FROM first_table WHERE id = 89) THEN
-    -- Insert the new row
-    INSERT INTO first_table (id, name) VALUES (89, 'Best School');
-    SELECT 'Row inserted successfully.' AS status;
-ELSE
-    SELECT 'Row with id 89 already exists. No insertion performed.' AS status;
-END IF;
+INSERT INTO hbtn_0c_0.first_table (id, name) VALUES (89, 'Best School');
