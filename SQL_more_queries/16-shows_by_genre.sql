89754590c9acdef9bcbc3aed81105d3f12e54782
Crisstaal <144152402+Crisstaal@users.noreply.github.com>
2024-02-21 00:31:04 -0600

    f

--- SQL_more_queries/16-shows_by_genre.sql
+++ SQL_more_queries/16-shows_by_genre.sql
@@ -1,6 +1,6 @@
 -- ists all shows, and all genres linked to that show, from the database
-SELECT tv_shows.title, COALESCE(tv_genres.name, 'NULL') AS genre
+SELECT tv_shows.title, COALESCE(tv_genres.name, 'NULL') AS name
 FROM tv_shows
 LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
 LEFT JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
-ORDER BY tv_shows.title ASC, genre ASC;
+ORDER BY tv_shows.title ASC, COALESCE(tv_genres.name, 'NULL') ASC;
