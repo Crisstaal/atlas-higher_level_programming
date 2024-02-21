7d32849cddf8b3b34f3592fdb17447d74e8aad70
Crisstaal <144152402+Crisstaal@users.noreply.github.com>
2024-02-20 21:35:49 -0600

    fix

--- SQL_introduction/4-first_table.sql
+++ SQL_introduction/4-first_table.sql
@@ -1,8 +1,18 @@
 -- 4-first_table.sql
 
-USE `hbtn_0c_0`;
+USE `hbtn_test_db_4`;
 
-CREATE TABLE IF NOT EXISTS first_table (
-    id INT,
-    name VARCHAR(256)
+-- Check if the table already exists
+SET @table_exists = (
+    SELECT COUNT(*)
+    FROM information_schema.tables
+    WHERE table_schema = DATABASE() AND table_name = 'first_table'
 );
+
+-- If the table doesn't exist, create it
+IF @table_exists = 0 THEN
+    CREATE TABLE first_table (
+        id INT,
+        name VARCHAR(256)
+    );
+END IF;
