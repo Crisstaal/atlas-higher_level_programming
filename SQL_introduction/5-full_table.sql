973bbeb02b6aeb0eddc4a57b7758ce28047c9bb1
Crisstaal <144152402+Crisstaal@users.noreply.github.com>
2024-02-20 21:56:38 -0600

    fix errors

--- SQL_introduction/5-full_table.sql
+++ SQL_introduction/5-full_table.sql
@@ -1,7 +1,2 @@
--- get_table_description.sql
-
--- Set the database name
-USE hbtn_0c_0;
-
--- Show the create table statement for first_table
-SHOW CREATE TABLE first_table;
+-- prints the full description of the table first_table from the database
+SHOW CREATE TABLE `first_table`;
