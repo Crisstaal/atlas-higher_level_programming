34c251aad1896babe6b6567d6ec78be8628719f5
Crisstaal <144152402+Crisstaal@users.noreply.github.com>
2024-02-20 22:55:02 -0600

    f

--- SQL_introduction/6-list_values.sql
+++ SQL_introduction/6-list_values.sql
@@ -1,10 +1,2 @@
-USE hbtn_0c_0;
-
--- Check if the table has records
-IF EXISTS (SELECT 1 FROM first_table LIMIT 1) THEN
-    -- Select all rows from the first_table
-    SELECT * FROM first_table;
-ELSE
-    -- Display a message if the table has no records
-    SELECT 'Table has no records.' AS message;
-END IF;
+-- lists all rows of the table first_table from the database hbtn_0c_0 in your MySQL server
+SELECT * FROM `first_table`;
