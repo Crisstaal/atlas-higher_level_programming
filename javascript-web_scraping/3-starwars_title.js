def7fb91d4a9fc8e12faa440b2a4ada996111286
Crisstaal <144152402+Crisstaal@users.noreply.github.com>
2024-04-02 16:08:44 -0500

    fix

--- javascript-web_scraping/3-starwars_title.js
+++ javascript-web_scraping/3-starwars_title.js
@@ -1,7 +1,14 @@
 #!/usr/bin/node
 
 const request = require('request');
-const url = 'https://swapi-api.hbtn.io/api/films/:id' + process.argv[2];
+const movieId = process.argv[2];
+const url = `https://swapi-api.hbtn.io/api/films/${movieId}`;
+
 request(url, function (error, response, body) {
-    console.log(error || JSON.parse(body).title);
+    if (error) {
+        console.error(error);
+    } else {
+        const movieData = JSON.parse(body);
+        console.log(movieData.title);
+    }
 });
